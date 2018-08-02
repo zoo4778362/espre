18b335180676ae6eef201b4065828637230335fb
zoo4778362 <zoo4778362@gmail.com>
2018-08-02 15:34:59 +0800

    1.增加从csv中读取点的功能，按行读取 2.区分不同数据大小的文件 3.修改原有sender逻辑

--- src/main/java/com/qiniu/qatest/es/core/DataSenderTest.java
+++ src/main/java/com/qiniu/qatest/es/core/DataSenderTest.java
@@ -10,6 +10,7 @@ import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
 import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
 import org.apache.jmeter.samplers.SampleResult;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -21,12 +22,20 @@ public class DataSenderTest extends AbstractJavaSamplerClient {
     private String repoName;
     private Auth auth;
     private Sender sender;
+    private String type;
+    private String path;//数据点读取路径
+    private String pathbigger;
+    private String patherror;
 
     @Override
     public void setupTest(JavaSamplerContext context) {
         ak = context.getParameter("ak");
         sk = context.getParameter("sk");
         repoName = context.getParameter("reponame");
+        type = context.getParameter("type");
+        path = context.getParameter("path");
+        pathbigger = context.getParameter("pathbigger");
+        patherror = context.getParameter("patherror");
         auth = Auth.create(ak, sk);
         sender = new ParallelDataSender(repoName, this.auth, 5);
     }
@@ -56,19 +65,49 @@ public class DataSenderTest extends AbstractJavaSamplerClient {
     @Override
     public Arguments getDefaultParameters() {
         Arguments params = new Arguments();
-        params.addArgument("ak", "null");
-        params.addArgument("sk", "null");
-        params.addArgument("reponame", null);
+        params.addArgument("ak", "七牛ak");
+        params.addArgument("sk", "七牛sk");
+        params.addArgument("reponame", "七牛logdb仓库名");
+        params.addArgument("type", "打点方式");//支持根据type选择打点方式  1.默认数据 2.文件读取正常数据 3.文件写入大数据 4.文件写入错误数据
+        params.addArgument("path", "文件读取路径");
+        params.addArgument("pathbigger", "大数据文件读取路径");//多次引用会有冲突
+        params.addArgument("patherror", "错误文件读取路径");
         return params;
     }
 
-    private void qiniuSender() {
-        //5个线程同时打点，每次打点数为100
+    private List<Point> createDate() {
+        int sendertype = Integer.parseInt(this.type);
+        switch (sendertype) {
+            case 1:
+                return defalutData();
+            case 2:
+                return pointsFromFile(this.path);
+            case 3:
+                return pointsFromFile(this.pathbigger);
+            case 4:
+                return pointsFromFile(this.patherror);
+        }
+        return new ArrayList<>();
+    }
+
+    /**
+     * type 1的默认数据。100个点
+     *
+     * @return
+     */
+    private List<Point> defalutData() {
         List<Point> points = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
             points.add(makePoint(i));
         }
-        SendPointError error = this.sender.send(points);
+        return points;
+    }
+
+    /**
+     * 发送数据点
+     */
+    private void qiniuSender() {
+        SendPointError error = this.sender.send(createDate());
         System.out.println(error.getExceptions());
     }
 
@@ -80,5 +119,43 @@ public class DataSenderTest extends AbstractJavaSamplerClient {
         p.append("f1", 4.5 + i);
         p.append("t", new Date());
         return p;
+    }
+
+    /**
+     * 根据path读取文件，转化为point点，注意文件格式
+     *
+     * @return
+     */
+    private List<Point> pointsFromFile(String path) {
+        File file = new File(path);
+        Point p;
+        BufferedReader reader = null;
+        String temp;
+        List<Point> points = new ArrayList<>();
+        if (this.path.isEmpty() || !file.exists() || null == file) {
+            return points;
+        }
+        int line = 1;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            while ((temp = reader.readLine()) != null) {
+                p = Point.fromPointString(temp);
+                points.add(p);
+                line++;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != reader) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return points;
     }
 }
